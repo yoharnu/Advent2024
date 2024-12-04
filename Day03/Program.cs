6500e3cd15a465accb9cd8a6fc90187a2d1822d9
yoharnu <dani91@gmail.com>
2024-12-04 18:39:07 -0500

    Day03: part 2

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -1,8 +1,14 @@
 ﻿using System.Text.RegularExpressions;
 
-var sampleFile = new StreamReader(new FileStream("./input/sample.txt", FileMode.Open));
-Console.WriteLine("Sample Solution:");
-ReadFile(sampleFile);
+var sampleFile1 = new StreamReader(new FileStream("./input/sample.txt", FileMode.Open));
+Console.WriteLine("Sample 1 Solution:");
+ReadFile(sampleFile1);
+
+Console.WriteLine();
+
+var sampleFile2 = new StreamReader(new FileStream("./input/sample2.txt", FileMode.Open));
+Console.WriteLine("Sample 2 Solution:");
+ReadFile(sampleFile2);
 
 Console.WriteLine();
 
@@ -14,8 +20,10 @@ static void ReadFile(StreamReader inputFile)
 {
     string contents = inputFile.ReadToEnd();
     int solutionOne = PartOne(contents);
+    int solutionTwo = PartTwo(contents);
 
     Console.WriteLine("Part 1: {0}", solutionOne);
+    Console.WriteLine("Part 2: {0}", solutionTwo);
 }
 
 static int PartOne(string contents)
@@ -29,6 +37,33 @@ static int PartOne(string contents)
         var a = int.Parse(groups[1]);
         var b = int.Parse(groups[2]);
         sum += a * b;
+    }
+    return sum;
+}
+
+static int PartTwo(string contents)
+{
+    Regex rx = new Regex("(mul)\\((\\d{1,3}),(\\d{1,3})\\)|(do)\\(\\)|(don't)\\(\\)");
+    var sum = 0;
+    var matches = rx.Matches(contents);
+    var enabled = true;
+    foreach (Match match in matches)
+    {
+        var groups = match.Groups.Values.Select(x => x.Value).ToList();
+        if (groups[1].Equals("mul") && enabled)
+        {
+            var a = int.Parse(groups[2]);
+            var b = int.Parse(groups[3]);
+            sum += a * b;
+        }
+        else if (groups[4].Equals("do"))
+        {
+            enabled = true;
+        }
+        else if (groups[5].Equals("don't"))
+        {
+            enabled = false;
+        }
     }
     return sum;
 }
