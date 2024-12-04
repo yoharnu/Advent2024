963db827bf839cedfeb1690b891fe13689de7510
yoharnu <dani91@gmail.com>
2024-12-04 15:16:41 -0500

    Day 02 Part 1

--- Day02/Program.cs
+++ Day02/Program.cs
@@ -10,5 +10,37 @@ ReadFile(inputFile);
 
 static void ReadFile(StreamReader inputFile)
 {
-    
+    string? line;
+    var safeCount = 0;
+    while ((line = inputFile.ReadLine()) != null)
+    {
+        var safe = true;
+        var increaseCount = 0;
+        var decreaseCount = 0;
+        List<int> report = line.Split().Select(x => int.Parse(x)).ToList();
+        for (int i = 1; i < report.Count; i++)
+        {
+            var diff = Math.Abs(report[i - 1] - report[i]);
+            if (diff < 1 || diff > 3)
+            {
+                safe = false;
+            }
+            if (report[i - 1] < report[i])
+            {
+                increaseCount++;
+            }
+            else if (report[i - 1] > report[i])
+            {
+                decreaseCount++;
+            }
+        }
+
+        if (increaseCount > 0 && decreaseCount > 0)
+            safe = false;
+
+        if (safe)
+            safeCount++;
+    }
+
+    Console.WriteLine("Part 1: {0}", safeCount);
 }
