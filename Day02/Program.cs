5850e7a07f7d5a4fb669ef6c9f33f427ea713b84
yoharnu <dani91@gmail.com>
2024-12-04 15:43:10 -0500

    Day02: extract part 1 into method

--- Day02/Program.cs
+++ Day02/Program.cs
@@ -11,36 +11,45 @@ ReadFile(inputFile);
 static void ReadFile(StreamReader inputFile)
 {
     string? line;
-    var safeCount = 0;
+    var safeCountOne = 0;
     while ((line = inputFile.ReadLine()) != null)
     {
-        var safe = true;
-        var increaseCount = 0;
-        var decreaseCount = 0;
         List<int> report = line.Split().Select(x => int.Parse(x)).ToList();
-        for (int i = 1; i < report.Count; i++)
-        {
-            var diff = Math.Abs(report[i - 1] - report[i]);
-            if (diff < 1 || diff > 3)
-            {
-                safe = false;
-            }
-            if (report[i - 1] < report[i])
-            {
-                increaseCount++;
-            }
-            else if (report[i - 1] > report[i])
-            {
-                decreaseCount++;
-            }
-        }
 
-        if (increaseCount > 0 && decreaseCount > 0)
-            safe = false;
+        var safeOne = PartOne(report);
 
-        if (safe)
-            safeCount++;
+        if (safeOne)
+            safeCountOne++;
     }
 
-    Console.WriteLine("Part 1: {0}", safeCount);
+    Console.WriteLine("Part 1: {0}", safeCountOne);
+}
+
+static bool PartOne(List<int> report)
+{
+    var safe = true;
+    var increaseCount = 0;
+    var decreaseCount = 0;
+
+    for (int i = 1; i < report.Count; i++)
+    {
+        var diff = Math.Abs(report[i - 1] - report[i]);
+        if (diff < 1 || diff > 3)
+        {
+            safe = false;
+        }
+        if (report[i - 1] < report[i])
+        {
+            increaseCount++;
+        }
+        else if (report[i - 1] > report[i])
+        {
+            decreaseCount++;
+        }
+    }
+
+    if (increaseCount > 0 && decreaseCount > 0)
+        safe = false;
+
+    return safe;
 }
