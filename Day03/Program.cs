742f11b0ba03177cda6e3b8fa950641eb855ff8c
yoharnu <dani91@gmail.com>
2024-12-04 18:23:55 -0500

    Day03: part 1

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -1,4 +1,6 @@
-﻿var sampleFile = new StreamReader(new FileStream("./input/sample.txt", FileMode.Open));
+﻿using System.Text.RegularExpressions;
+
+var sampleFile = new StreamReader(new FileStream("./input/sample.txt", FileMode.Open));
 Console.WriteLine("Sample Solution:");
 ReadFile(sampleFile);
 
@@ -10,5 +12,23 @@ ReadFile(inputFile);
 
 static void ReadFile(StreamReader inputFile)
 {
+    string contents = inputFile.ReadToEnd();
+    int solutionOne = PartOne(contents);
 
+    Console.WriteLine("Part 1: {0}", solutionOne);
+}
+
+static int PartOne(string contents)
+{
+    Regex mul = new Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)");
+    int sum = 0;
+    var matches = mul.Matches(contents);
+    foreach (Match match in matches)
+    {
+        var groups = match.Groups.Values.Select(x => x.Value).ToList();
+        var a = int.Parse(groups[1]);
+        var b = int.Parse(groups[2]);
+        sum += a * b;
+    }
+    return sum;
 }
