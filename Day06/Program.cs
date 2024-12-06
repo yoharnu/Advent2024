3f8060d6fed11d69babef59d474f89128d47832a
yoharnu <dani91@gmail.com>
2024-12-06 07:27:59 -0500

    Day06: initial setup

---
+++ Day06/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Sample Solution:");
+using (var sampleFile = new StreamReader(new FileStream("./input/sample.txt", FileMode.Open)))
+    ReadFile(sampleFile);
+
+Console.WriteLine();
+
+Console.WriteLine("Final Solution:");
+using (var inputFile = new StreamReader(new FileStream("./input/input.txt", FileMode.Open)))
+    ReadFile(inputFile);
+
+static void ReadFile(StreamReader inputFile)
+{
+
+}
