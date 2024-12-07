bd21fcc7a1ab56af154db82f9e3f0ed443faf990
yoharnu <dani91@gmail.com>
2024-12-07 07:51:37 -0500

    Day07: initial setup

---
+++ Day07/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+Console.WriteLine("Sample Solution:");
+using (var sampleFile = new StreamReader(new FileStream("./input/sample.txt", FileMode.Open)))
+    ReadFile(sampleFile);
+
+Console.WriteLine();
+
+Console.WriteLine("Final Solution:");
+using (var inputFile = new StreamReader(new FileStream("./input/input.txt", FileMode.Open)))
+    ReadFile(inputFile);
+
+static void ReadFile(StreamReader inputFile)
+{
+    long startTime = Stopwatch.GetTimestamp();
+    int solutionOne = PartOne();
+    TimeSpan elapsedTime = Stopwatch.GetElapsedTime(startTime);
+    Console.WriteLine("Part 1: {0}\t(completed in {1}s)", solutionOne, elapsedTime.TotalSeconds);
+}
+
+static int PartOne()
+{
+    return 0;
+}
