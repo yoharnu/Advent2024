8116bee8e136e406743e483a58830290a6632eef
yoharnu <dani91@gmail.com>
2024-12-23 14:30:45 -0500

    feat: Add FrequencyGrid class for handling grids with frequency antennas for Day08
    - Implemented the `FrequencyGrid` class that inherits from the `Grid` class.
    - Added constructors to initialize the `FrequencyGrid` from a list of lists of characters or a list of strings.
    - Added a `ConvertToCharGrid` method to convert a list of strings to a list of lists of characters.
    - Implemented the `GetDistinctValues` method to retrieve distinct values from the grid.
    
    This addition provides functionality for handling grids with frequency analysis, allowing for easy retrieval of distinct values in the grid.

---
+++ Day08/FrequencyGrid.cs
@@ -0,0 +1,29 @@
+﻿using Business;
+
+namespace Day08;
+
+public class FrequencyGrid : Grid
+{
+    public FrequencyGrid(List<List<char>> grid) : base(grid)
+    {
+    }
+    public FrequencyGrid(List<string> grid) : base(ConvertToCharGrid(grid))
+    {
+    }
+
+    private static List<List<char>> ConvertToCharGrid(List<string> grid) => grid.Select(row => row.ToList()).ToList();
+
+
+    public List<char> GetDistinctValues()
+    {
+        var distinctValues = new HashSet<char>();
+        for (int y = 0; y < Height; y++)
+        {
+            for (int x = 0; x < Width; x++)
+            {
+                distinctValues.Add(GetValueAt(x, y));
+            }
+        }
+        return distinctValues.ToList();
+    }
+}
