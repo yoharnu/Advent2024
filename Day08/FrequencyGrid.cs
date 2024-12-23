5d7535e01455927d1e023874ff2f7e8dbe5cd13c
yoharnu <dani91@gmail.com>
2024-12-23 14:59:04 -0500

    add docs to FrequencyGrid

--- Day08/FrequencyGrid.cs
+++ Day08/FrequencyGrid.cs
@@ -4,15 +4,35 @@ namespace Day08;
 
 public class FrequencyGrid : Grid
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FrequencyGrid"/> class with the specified grid and optional blank space character.
+    /// </summary>
+    /// <param name="grid">The grid of characters.</param>
+    /// <param name="blankSpace">The character to be treated as a blank space. Default is ' '.</param>
     public FrequencyGrid(List<List<char>> grid, char blankSpace = ' ') : base(grid, blankSpace)
     {
     }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FrequencyGrid"/> class with the specified grid and optional blank space character.
+    /// </summary>
+    /// <param name="grid">The list of strings representing the grid.</param>
+    /// <param name="blankSpace">The character to be treated as a blank space. Default is ' '.</param>
     public FrequencyGrid(List<string> grid, char blankSpace = ' ') : base(ConvertToCharGrid(grid), blankSpace)
     {
     }
 
+    /// <summary>
+    /// Converts a list of strings to a list of list of characters.
+    /// </summary>
+    /// <param name="grid">The list of strings representing the grid.</param>
+    /// <returns>A list of list of characters representing the grid.</returns>
     private static List<List<char>> ConvertToCharGrid(List<string> grid) => grid.Select(row => row.ToList()).ToList();
 
+    /// <summary>
+    /// Gets the distinct values from the grid, excluding the blank space character.
+    /// </summary>
+    /// <returns>A list of distinct characters present in the grid, excluding the blank space character.</returns>
     public List<char> GetDistinctValues()
     {
         var distinctValues = new HashSet<char>();
