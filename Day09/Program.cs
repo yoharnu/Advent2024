978582b84ecd959cac36e1b5b7dcab9f76007768
yoharnu <dani91@gmail.com>
2024-12-24 00:29:29 -0500

    create new map for each part of day09 problem

--- Day09/Program.cs
+++ Day09/Program.cs
@@ -17,12 +17,11 @@ static void ReadFile(StreamReader inputFile)
     Console.Write("Reading input...");
     var stopwatch = Stopwatch.StartNew();
     var lines = inputFile.ReadToEnd();
-    DiskMap map = new DiskMap(lines);
     stopwatch.Stop();
     Console.WriteLine(" Done in {0} seconds", stopwatch.Elapsed.TotalSeconds);
 
     stopwatch.Restart();
-    long solutionOne = PartOne(map);
+    long solutionOne = PartOne(new DiskMap(lines));
     stopwatch.Stop();
     Console.WriteLine("Part 1: {0}\t(completed in {1}s)", solutionOne, stopwatch.Elapsed.TotalSeconds);
 }
