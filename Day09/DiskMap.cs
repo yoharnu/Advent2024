6459d3a215e0cd12d10f08f0983f8fed809c3efc
yoharnu <dani91@gmail.com>
2024-12-24 00:24:24 -0500

    Day09: part 1

---
+++ Day09/DiskMap.cs
@@ -0,0 +1,80 @@
+﻿using System.Text;
+
+namespace Day09;
+
+public class DiskMap
+{
+    private readonly string _diskmap;
+    private List<int> _decompressed;
+    public DiskMap(string diskmap)
+    {
+        _diskmap = diskmap;
+        _decompressed = new List<int>(_diskmap.Length);
+        Decompress();
+    }
+
+    public long Checksum()
+    {
+        long sum = 0;
+        for (int i = 0; i < _decompressed.Count; i++)
+        {
+            if (_decompressed[i] == -1) continue;
+
+            sum += i * _decompressed[i];
+        }
+        return sum;
+    }
+
+    private void Decompress()
+    {
+        int id = 0;
+        for (int i = 0; i < _diskmap.Length; i++)
+        {
+            if (i % 2 == 0) // file size
+            {
+                for (int j = 0; j < int.Parse(_diskmap[i].ToString()); j++)
+                {
+                    _decompressed.Add(id);
+                }
+                id++;
+            }
+            else
+            {
+                for (int j = 0; j < int.Parse(_diskmap[i].ToString()); j++)
+                {
+                    _decompressed.Add(-1);
+                }
+            }
+        }
+    }
+
+    public void Display()
+    {
+        var sb = new StringBuilder(_decompressed.Count);
+        foreach (var x in _decompressed)
+        {
+            sb.Append(x == -1 ? '.' : x.ToString());
+        }
+        Console.WriteLine(sb.ToString());
+    }
+
+    public void Defrag()
+    {
+        int fileCount = _decompressed.Count(x => x != -1);
+        int lastIndex = _decompressed.Count - 1;
+
+        for (int i = 0; i < fileCount; i++)
+        {
+            if (_decompressed[i] == -1)
+            {
+                while (_decompressed[lastIndex] == -1)
+                {
+                    lastIndex--;
+                }
+                _decompressed[i] = _decompressed[lastIndex];
+                _decompressed[lastIndex] = -1;
+                lastIndex--;
+            }
+        }
+    }
+}
---
+++ Day09/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using Business;
+using Day09;
+
+Console.WriteLine("Sample Solution:");
+using (var sampleFile = new StreamReader(new FileStream("./input/sample.txt", FileMode.Open)))
+    ReadFile(sampleFile);
+
+Console.WriteLine();
+
+Console.WriteLine("Final Solution:");
+using (var inputFile = new StreamReader(new FileStream("./input/input.txt", FileMode.Open)))
+    ReadFile(inputFile);
+
+static void ReadFile(StreamReader inputFile)
+{
+    Console.Write("Reading input...");
+    var stopwatch = Stopwatch.StartNew();
+    var lines = inputFile.ReadToEnd();
+    DiskMap map = new DiskMap(lines);
+    stopwatch.Stop();
+    Console.WriteLine(" Done in {0} seconds", stopwatch.Elapsed.TotalSeconds);
+
+    stopwatch.Restart();
+    long solutionOne = PartOne(map);
+    stopwatch.Stop();
+    Console.WriteLine("Part 1: {0}\t(completed in {1}s)", solutionOne, stopwatch.Elapsed.TotalSeconds);
+}
+
+static long PartOne(DiskMap map)
+{
+    map.Defrag();
+    return map.Checksum();
+}
