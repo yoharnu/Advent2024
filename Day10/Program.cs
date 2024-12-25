f75af83dc1d760f41e84d1af243336c236e00396
yoharnu <dani91@gmail.com>
2024-12-24 22:27:01 -0500

    Day10: Part 1 complete

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -28,5 +28,46 @@ static void ReadFile(StreamReader inputFile)
 static long PartOne(Grid map)
 {
     var trailheads = map.FindLocationsWithValue('0');
-    return 0;
+    var peakSum = 0;
+    foreach (var trailhead in trailheads)
+    {
+        HashSet<Grid.Location> peaks = new HashSet<Grid.Location>();
+        var stack = new Stack<Grid.Location>();
+        stack.Push(trailhead);
+        while (stack.Count > 0)
+        {
+            var current = stack.Pop();
+
+            if (map.IsOutOfBounds(current))
+                continue;
+
+            var currentValue = int.Parse(current.Value.ToString());
+
+            if (currentValue == 9)
+            {
+                peaks.Add(current);
+            }
+            else
+            {
+                var neighbors = new List<Grid.Location> { current.GetNeighborDown(), current.GetNeighborUp(), current.GetNeighborLeft(), current.GetNeighborRight() };
+
+                for (int i = 0; i < neighbors.Count; i++)
+                {
+                    var neighbor = neighbors[i];
+
+                    if (map.IsOutOfBounds(neighbor)) continue;
+
+                    neighbor.Value = map.GetValueAt(neighbor.X, neighbor.Y);
+
+                    if (int.Parse(neighbor.Value.ToString()) == int.Parse(current.Value.ToString()) + 1)
+                    {
+                        stack.Push(neighbor);
+                    }
+                }
+            }
+        }
+
+        peakSum += peaks.Count;
+    }
+    return peakSum;
 }
