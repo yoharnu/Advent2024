b917ce1dc64bdd5bae16e19a419812389ddcf3f9
yoharnu <dani91@gmail.com>
2024-12-24 21:26:31 -0500

    Day10: initial setup

---
+++ Day10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using Business;
+
+Console.WriteLine("Sample Solution:");
+using (var sampleFile = new StreamReader(new FileStream("./input/sample.txt", FileMode.Open)))
+    ReadFile(sampleFile);
+
+Console.WriteLine();
+
+Console.WriteLine("Final Solution:");
+using (var inputFile = new StreamReader(new FileStream("./input/input.txt", FileMode.Open)))
+    ReadFile(inputFile);
+
+static void ReadFile(StreamReader inputFile)
+{
+    Console.Write("Reading input...");
+    var stopwatch = Stopwatch.StartNew();
+    var map = new Grid(Helper.ReadAllLines(inputFile));
+    stopwatch.Stop();
+    Console.WriteLine(" Done in {0} seconds", stopwatch.Elapsed.TotalSeconds);
+
+    stopwatch.Restart();
+    long solutionOne = PartOne(map);
+    stopwatch.Stop();
+    Console.WriteLine("Part 1: {0}\t(completed in {1}s)", solutionOne, stopwatch.Elapsed.TotalSeconds);
+}
+
+static long PartOne(Grid map)
+{
+    var trailheads = map.FindLocationsWithValue('0');
+    return 0;
+}
